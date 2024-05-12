95386a88e896fe9e84acda8b6e445f64190a4b4a
William Caban <william.caban@gmail.com>
2024-05-11 23:56:20 -0400

    Update to build with latest UBI image from NVIDIA

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 OLLAMA_VERSION=$(curl -s "https://api.github.com/repos/ollama/ollama/releases/latest" | jq -r .name)
 
 echo ${OLLAMA_VERSION} > VERSION
+echo "VERSION=${OLLAMA_VERSION}" > OLLAMA_VERSION
 
-podman build --no-cache --build-arg=VERSION=${OLLAMA_VERSION} -t quay.io/wcaban/ollama:latest -f Containerfile.build
+podman build --no-cache --build-arg OLLAMA_VERSION=${OLLAMA_VERSION} -t quay.io/wcaban/ollama:latest -f Containerfile.build
 podman tag quay.io/wcaban/ollama:latest quay.io/wcaban/ollama:${OLLAMA_VERSION}
 
 # Moved to upload.sh
